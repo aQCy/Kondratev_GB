11d52e4e4da49e70b5c57d8c36599047b08dcc08
aQCy <rox4388@gmail.com>
2020-12-15 02:50:44 +1000

    Initial commit

---
+++ Hello World/Hello World/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main()
+        {
+            string UserName = Environment.UserName;
+            Console.WriteLine("Привет, {0}, сегодня {1}", UserName, DateTime.Now);
+            
+        }
+    }
+}
